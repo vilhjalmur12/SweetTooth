ba2dc5c050930d2e77c656ed4b51dd3392da7c14
IvarKristinn <hallssonivarkristinn@gmail.com>
2018-10-04 15:00:32 +0000

    Fixed indent and added README

---
+++ README.md
@@ -0,0 +1,14 @@
+# README
+# SC-T-514-VEFT
+### Small Assignment 3
+This README is about our project SweetTooth  
+implemented using Express in NodeJS with JavaScript.
+
+## To build
+Needs the newest NodeJs (https://nodejs.org/en/).  
+Needs npm init -y and nmp install --save express body-parser.  
+To build and run node index.js  
+
+## Team Members
+Ívar Kristinn Hallsson: ivar17@ru.is  
+Vilhjálmur Rúnar Vilhjálmsson: vilhjalmur12@ru.is
--- services/OfferService.js
+++ services/OfferService.js
@@ -4,14 +4,14 @@ const candyService = require("../services/CandyService");
 const OfferService = () => {
   //return all offers with candies
     const getAllOffers = () => {
-    const retValue = offerData;
-    retValue.forEach(offer => {
-      offer.candies.forEach((candy, index) => {
-        offer.candies[index] = candyService.getCandyById(candy);
-      })
-    });
-      return retValue;
-    };
+      const retValue = offerData;
+      retValue.forEach(offer => {
+        offer.candies.forEach((candy, index) => {
+          offer.candies[index] = candyService.getCandyById(candy);
+        })
+      });
+        return retValue;
+      };
 
     return {
         getAllOffers
